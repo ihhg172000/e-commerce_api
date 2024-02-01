33e100d4295b544386748d8f59a5e9a1b5dc2e65
Islam Hany <ihhg.developer@gmail.com>
2024-02-01 21:41:54 +0200

    feat: add category validators

--- routers/categories-router.js
+++ routers/categories-router.js
@@ -1,18 +1,20 @@
 const { Router } = require("express");
 const categoriesController = require("../controllers/categories-controller");
 const methodNotAllowedHandler = require("../middelwares/method-not-allowed-handler");
+const validate = require("../middelwares/validate-middelware");
+const categoryValidators = require("../validators/category-validators");
 
 const router = Router();
 
 router
   .route("/")
   .get(categoriesController.retrieveAll)
-  .post(categoriesController.createOne);
+  .post(validate(categoryValidators()), categoriesController.createOne);
 
 router
   .route("/:id")
   .get(categoriesController.retrieveOne)
-  .patch(categoriesController.updateOne)
+  .patch(validate(categoryValidators(true)), categoriesController.updateOne)
   .delete(categoriesController.deleteOne);
 
 router.use(methodNotAllowedHandler);
